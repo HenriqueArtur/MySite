615f0771d0def2c7d4da52e0c78a62892f83f79b
Henrique Artur <contato@henriqueartur.com>
2025-01-13 20:59:38 -0300

    🎉 begin MySite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
+        brand: 'var(--brand-color)',
+        'brand-text-1': 'var(--brand-color-text-1)',
+        'brand-text-2': 'var(--brand-color-text-2)',
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        'brand-gradient': 'var(--brand-color-bg)',
       },
     },
   },
