15ff149308831ae5c80da3d6535817eac2fd16df
Henrique Artur <contato@henriqueartur.com>
2025-01-17 20:55:53 -0300

    🚧 footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-start">
+      <div className="max-w-4xl">
+        <span>Henrique Artur © 2025</span>
+      </div>
+    </footer>
+  )
+}
