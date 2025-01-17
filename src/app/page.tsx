d8b6d274c1555f2f814e1f6705907b30906587e4
Henrique Artur <contato@henriqueartur.com>
2025-01-17 20:56:01 -0300

    🚧 home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,21 @@
+import Clock from "@/components/clock";
+import Footer from "@/components/footer";
+
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans text-lg">
-      <main className="flex flex-col gap-4">
+      <main className="flex flex-col gap-3">
         <div>
-          <p>👋 Hello, I&apos;am <span className="text-brand">Henrique Artur</span>!</p>
+          <Clock />
+          <h1 className="text-2xl font-bold">Hello, I&apos;am <span className="text-brand">Henrique Artur</span> 👋</h1>
           {/* database: https://filess.io/ */}
           <p>I create <u>great experiences</u> by building <u>better software</u>.</p>
         </div>
         <div>
           <p className="text-brand-text-2 text-base">You are the <u>[position]th</u> awesome person 🤩 to visit my site.</p>
         </div>
-        <div>
-          <p>My 3 principles:</p>
-          <ul className="text-brand-text-2 list-disc list-inside">
-            <li>Simplicity</li>
-            <li>Quality</li>
-            <li>Experience</li>
-          </ul>
-        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a className="text-brand-text-2 text-base hover:underline" target="_blank" href="https://github.com/HenriqueArtur/MySite">This site project</a>
-      </footer>
+      <Footer />
     </div >
   );
 }
